02ddf60812bdda2ae9fbb2dce43623d8f8bb7750
ahmetkaraca <ahmetkaraca2012@gmail.com>
2023-05-12 13:59:13 +0300

    php dosyası iptal

--- docs/forms/contact.php
+++ docs/forms/contact.php
@@ -7,6 +7,8 @@
   */
 
   // Replace contact@example.com with your real receiving email address
+
+  /*
   $receiving_email_address = 'contact@example.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
@@ -22,6 +24,7 @@
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
+  */
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
@@ -32,10 +35,11 @@
     'port' => '587'
   );
   */
-
+/*
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
   echo $contact->send();
 ?>
+*/
